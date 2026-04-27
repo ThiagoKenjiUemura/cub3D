f3e598904c44525b01150fe75e5c1b846b679bcc
Thiago Kenji Uemura <tkenji-u@c1r9p4.42sp.org.br>
2026-04-27 18:09:42 -0300

    Structures made

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,52 @@
 /*   By: tkenji-u <tkenji-u@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/24 18:04:56 by tkenji-u          #+#    #+#             */
-/*   Updated: 2026/04/24 18:04:57 by tkenji-u         ###   ########.fr       */
+/*   Updated: 2026/04/27 18:08:17 by tkenji-u         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CUB3D_H
+# define CUB3D_H
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <math.h>
+#include <mlx.h>
+#include "libft.h"
+
+typedef struct s_map
+{
+	char	*n_texture;
+	char	*s_texture;
+	char	*w_texture;
+	char	*e_texture;
+	char	**map;
+	int		floor_rgb[3];
+	int		ceiling_rgb[3];
+	size_t	width;
+	size_t	height;
+}			t_map;
+
+typedef struct s_player
+{
+	double	x;
+	double	y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+}			t_player;
+
+typedef struct s_game
+{
+	t_map		map;
+	t_player	player;
+	void		*mlx_ptr;
+	void		*win_ptr;
+}				t_game;
+
+#endif
